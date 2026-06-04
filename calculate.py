74cd55dde187c8b51438beb2f64260b981bad46c
Saini Disha Mukesh <dishasaini2607@gmail.com>
2026-06-04 22:26:26 +0530

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,20 +1,24 @@
-add=n1+n2
-sub=n1-n2
-multi=n1*n2
-div=n1/n2
-n1=input(print("Enter number 1:"))
-n2-input(print("Enter number 2:"))
+n1=int(input(("Enter number 1:")))
+n2=int(input(("Enter number 2:")))
 
-op=input(print("which operation you want to perform? enter 1 for addition 2 for substractions 3 for multiplication 4 for division:"))
-if op==1:
-  result=input(add)
-if op==2:
-   result=input(sub)
-if op==3:
-   result=input(multi)
-if op==4:
-  if n2==0:
-    print("error")
-  else:
-    result=input(div)
-print("your answer is", result)
+op=input(("which operation you want to perform? enter 1 for addition 2 for substractions 3 for multiplication 4 for division:"))
+if op=="1":
+    add=n1+n2
+    result=add
+    print("your answer is",result)
+elif op=="2":
+    sub=n1-n2
+    result=sub
+    print("your answer is",result)
+elif op=="3":
+    multi=n1*n2
+    result=multi
+    print("your answer is",result)
+elif op=="4":
+    if n2==0:
+        print("error division by zero not applicable")
+    else:
+        div=n1/n2
+        result=div
+        print("your answer is",result)
+
